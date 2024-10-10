5336811d095087e32a9e9beaed2609676ab57e65
ceres <ceres.bezuidenhout@trintel.co.za>
2024-10-10 16:14:09 +0200

    Read uci lists;Commit lock file

--- libuci-sys/src/lib.rs
+++ libuci-sys/src/lib.rs
@@ -28,7 +28,7 @@ pub use bindings::{
     uci_commit, uci_context, uci_del_list, uci_delete, uci_delta, uci_element, uci_export,
     uci_flags, uci_free_context, uci_get_errorstr, uci_hash_options, uci_import, uci_list,
     uci_list_configs, uci_load, uci_lookup_next, uci_lookup_ptr, uci_option, uci_option_type,
-    uci_option_type_UCI_TYPE_STRING, uci_package, uci_parse_argument, uci_parse_context,
+    uci_option_type_UCI_TYPE_STRING, uci_option_type_UCI_TYPE_LIST, uci_package, uci_parse_argument, uci_parse_context,
     uci_parse_option, uci_parse_ptr, uci_parse_section, uci_perror, uci_ptr,
     uci_ptr_UCI_LOOKUP_COMPLETE, uci_rename, uci_reorder_section, uci_revert, uci_save,
     uci_section, uci_set, uci_set_backend, uci_set_confdir, uci_set_savedir, uci_type,
